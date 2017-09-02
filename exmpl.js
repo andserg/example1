3c63a86ecb6b0a9f762d5023e204ed530323ec34
Sergio <androniksergio@gmail.com>
2017-09-02 17:43:11 +0300

    Initial commit

---
+++ exmpl.js
@@ -0,0 +1,51 @@
+console.log("Create array");
+var a = "a";
+var b = "b";
+var c = "c";
+var d = "d";
+var array = [];
+
+for(var i=1; i <= 100 ; i++) {
+	array[i] = i;
+	console.log(array[i]);
+}
+
+console.log("Array was created");
+
+for(var i=1; i <= 100 ; i++) {
+	if(array[i] % 3 === 0 && array[i] % 5 === 0) {
+		array[i] = a + b;
+	}
+	if(array[i] % 3 === 0 && array[i] % 7 === 0) {
+		array[i] = a + c;
+	}
+	if(array[i] % 3 === 0 && array[i] % 11 === 0) {
+		array[i] = a + d;
+	}
+	if(array[i] % 5 === 0 && array[i] % 7 === 0) {
+		array[i] = b + c;
+	}
+	if(array[i] % 5 === 0 && array[i] % 11 === 0) {
+		array[i] = b + d;
+	}
+	if(array[i] % 7 === 0 && array[i] % 11 === 0) {
+		array[i] = c + d;
+	}
+	if(array[i] % 3 === 0) {
+		array[i] = a;
+	}
+	if(array[i] % 5 === 0) {
+		array[i] = b;
+	}
+	if(array[i] % 7 === 0) {
+		array[i] = c;
+	}
+	if(array[i] % 11 === 0) {
+		array[i] = d;
+	}
+}
+
+console.log("Print array with replacement.");
+for(var i=1; i <= 100 ; i++) {
+	console.log(array[i]);
+}
